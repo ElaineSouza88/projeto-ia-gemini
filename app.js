ae40337a94520891c7da447dfb333ef85e0bba74
Elaine Souza <elainehandball@yahoo.com.br>
2024-09-06 14:31:10 -0300

    Update

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
 function pesquisar() {
 
-    let section = document.getElementById('atletas')
-    let resultados = "" // variável criada para armazenar os resultados da busca
+    let section = document.getElementById('atletas') //trás a seção onde os resultados são exibidos
 
-    //Para cada dado dentro da lista faça:
+    let campoPesquisa = document.getElementById('campo-pesquisa').value; //trás somente o dado relacionado ao item que o usuário digitar
 
+    if (!campoPesquisa) {  //Condição criada caso usuário digite valor inválido
+        section.innerHTML = "<p>Nada foi encontrado !</p>"
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase() //vai colocar todos os dados do campo de pesquisa em minúsculas
+
+    console.log(campoPesquisa); //Exibe dados no console do navegador (Inspecionar)
+
+    let resultados = "" // variáveis criada para armazenar os resultados da busca
+    let titulo = ""
+    let descricao = ""
+    let tags = ""
+
+//Laço de repetição:
 for (let dado of dados) {  //Para cada dado da lista de variáveis de dados.
-    resultados += `  
-    <div id="resultado-pesquisa">
-        <h2><strong>${dado.titulo}</strong></h2>
-        <p>${dado.descricao}</p><br>
-        <h3>Títulos</h3>
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tag = dado.tags.toLowerCase()
+
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        resultados += `  
+            <div id="resultado-pesquisa">
+            <h2><strong>${dado.titulo}</strong></h2>
+            <p>${dado.descricao}</p><br>
+            <h3>Títulos</h3>
         <ul>
             <li><strong>1988:</strong> Sua primeira conquista veio ao volante da McLaren-Honda, um carro dominador daquela temporada. Senna mostrou um desempenho excepcional, combinando velocidade com consistência.</li>
             <li><strong>1990:</strong> Em uma temporada marcada pela rivalidade com Alain Prost, Senna conseguiu superar o francês e conquistar seu segundo título mundial.</li>
             <li><strong>1991:</strong> Em seu ano mais dominante, Senna conquistou seu terceiro título mundial com quatro vitórias nas primeiras quatro corridas.<br></li>
         </ul>
-        <p>Saiba mais detalhes em <a href="https://senna.com/" target="_blank" rel="external">Senna.com</a>.</p>
-        </div>
-    
-`
+        <p>Para mais detalhes <a href=${dado.link} target="_blank" rel="external"> clique aqui</a>.</p>
+        </div>   
+    `;
+        }
 }
-section.innerHTML = resultados
+
+if (!resultados) {
+    resultados = "<p>Nada foi encontrado !</p>"
+}
+
+section.innerHTML = resultados; //atribui resultados referente a seção HTML
 }
--- dados.js
+++ dados.js
@@ -4,36 +4,42 @@ let dados = [
         descricao : "Senna era conhecido por sua habilidade excepcional ao volante, determinação e paixão pelas corridas. Seu estilo de pilotagem agressivo e preciso o tornou um dos maiores ídolos do automobilismo mundial. Tragicamente, faleceu durante o Grande Prêmio de San Marino de 1994, deixando um legado que o imortalizou no esporte.",
         titulos : "1988: Sua primeira conquista veio ao volante da McLaren-Honda, um carro dominador daquela temporada. Senna mostrou um desempenho excepcional, combinando velocidade com consistência. 1990: Em uma temporada marcada pela rivalidade com Alain Prost, Senna conseguiu superar o francês e conquistar seu segundo título mundial.1991: Em seu ano mais dominante, Senna conquistou seu terceiro título mundial com quatro vitórias nas primeiras quatro corridas.",
         link : "https://senna.com/",
+        tags : "GOAT goat piloto pilotos legado"
     },
     {
         titulo: "Nelson Piquet",
         descricao: "Três vezes campeão mundial de Fórmula 1, Nelson Piquet era conhecido por sua inteligência e habilidade técnica. Seu estilo de pilotagem era caracterizado pela precisão e pela capacidade de extrair o máximo do carro.",
         titulos: "1981, 1983 e 1987: Conquistou seus títulos mundiais pilotando por equipes como Brabham e Williams.",
-        link: "https://pt.wikipedia.org/wiki/Nelson_Piquet"
+        link: "https://pt.wikipedia.org/wiki/Nelson_Piquet",
+        tags : "piloto pilotos tricampeao Piquet"
     },
     {
         titulo: "Emerson Fittipaldi",
         descricao: "O primeiro brasileiro a conquistar o título mundial de Fórmula 1, em 1972. Fittipaldi era um piloto completo, com grande habilidade e versatilidade. Além da Fórmula 1, conquistou títulos na Indy 500 e nas 24 Horas de Le Mans.",
         titulos: "1972 e 1974: Conquistou seus títulos mundiais pilotando pela Lotus.",
-        link: "https://pt.wikipedia.org/wiki/Emerson_Fittipaldi"
+        link: "https://pt.wikipedia.org/wiki/Emerson_Fittipaldi",
+        tags : "primeiro campeao brasileiro piloto"
     },
     {
         titulo: "Chico Landi",
         descricao: "Considerado um dos pioneiros do automobilismo brasileiro, Chico Landi foi o primeiro brasileiro a competir na Fórmula 1, em 1951. Apesar de não ter conquistado títulos mundiais, deixou um legado importante para o automobilismo nacional.",
         titulos: "Os principais fatores que impediram o brasileiro a conquistar títulos na Fórmula 1 foram: Disputou apenas seis Grandes Prêmios, o que limitou suas chances de pontuar e brigar por um título mundial. Os carros brasileiros da época não eram tão competitivos quanto os europeus, o que dificultava a vida de Landi nas pistas. A falta de recursos financeiros impedia Landi de ter um carro competitivo e de disputar todas as corridas do campeonato.",
-        link: "https://www.museudoautomobilismo.org.br/chico-landi/"
+        link: "https://pt.wikipedia.org/wiki/Chico_Landi",
+        tags : "pioneiro piloto legado"
     },
     {
         titulo: "Rubens Barrichello",
         descricao: "Um dos pilotos brasileiros com mais corridas disputadas na Fórmula 1, Rubens Barrichello era conhecido por sua consistência e lealdade às equipes. Apesar de não ter conquistado o título mundial, alcançou diversos pódios e vitórias.",
         titulos: "11 vitórias, 68 pódios, 14 pole positions",
-        link: "https://pt.wikipedia.org/wiki/Rubens_Barrichello"
+        link: "https://pt.wikipedia.org/wiki/Rubens_Barrichello",
+        tags : "piloto legado schumacher ferrari"
     },
     {
         titulo: "Felipe Massa",
         descricao: "É um dos maiores pilotos brasileiros de Fórmula 1. Vice-campeão mundial em 2008, Massa foi protagonista de uma das finais mais controversas da história da categoria. Sua carreira, marcada pela lealdade e pela paixão, o tornou um ídolo para milhões de fãs ao redor do mundo.",
         titulos: "Mesmo estando por bastante tempo na Fórmula 1, Felipe Massa não conquistou títulos mundiais nessa categoria, e alguns dos principais fatores foram:Temporada dramática de 2008: Disputou o campeonato ponto a ponto com Lewis Hamilton até a última corrida da temporada. No entanto, uma reviravolta no final da prova em Interlagos fez com que Hamilton se sagrasse campeão por apenas um ponto de diferença. Massa ainda contesta o resultado desta corrida, alegando que houve manipulações para favorecer o piloto inglês, fora a briga judicial com a FIA referente à batida de Nelsinho Piquet no GP de Singapura que o prejudicou na classificação do mesmo ano.Concorrência forte: Ao longo de sua carreira, Massa enfrentou uma concorrência fortíssima, com pilotos como Michael Schumacher, Fernando Alonso e Lewis Hamilton.Problemas de confiabilidade: Em algumas temporadas, Massa sofreu com problemas de confiabilidade em seus carros, o que o prejudicou na disputa pelo título.Acidentes: O piloto brasileiro também sofreu alguns acidentes graves ao longo de sua carreira, como o acidente em Hungaroring em 2009, que o deixou fora das pistas por um longo período.",
-        link: "http://felipemassa.com.br/"
+        link: "http://felipemassa.com.br/",
+        tags : "piloto 2008 processo FIA titulo perdido Nelsinho Piquet"
     }
 ];
 
